04217c892e9aa834b7a328c980bead1276ad1544
Valerii <v.du@dcjudgement.com>
2022-01-24 14:45:33 +0200

    Initial Commit

---
+++ Brain trainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brain trainer
+//
+//  Created by Valerii Dulia on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
